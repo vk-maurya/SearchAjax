018e94b24ed109632dafe1508b90103a666f79b8
Vijay Kumar Maurya <33961671+vk-maurya@users.noreply.github.com>
2018-01-22 11:39:53 +0530

    SQL

---
+++ search.sql
@@ -0,0 +1,13 @@
+create table simple(
+	
+	id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	name varchar(30),
+	email varchar(30),
+	city varchar(30),
+	gender varchar(10)
+);
+
+
+
+
+SELECT * from SIMPLE
